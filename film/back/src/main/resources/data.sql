2b93ee3b55ea61813b20b012d18e08bfa77da40f
n0uben <benjamin.noureux@gmail.com>
2023-03-09 22:41:42 +0100

    ajoute vrais films en bdd

--- film/back/src/main/resources/data.sql
+++ film/back/src/main/resources/data.sql
@@ -1,3 +1,33 @@
-INSERT INTO movie (nom, description, date_sortie, duree) VALUES ('test', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam luctus efficitur iaculis. Maecenas eu dolor sit amet nisl pellentesque dapibus. Proin lobortis, odio sit amet commodo mattis, odio ex porttitor nunis magna, a fermentum urna tempus eu.', '2022-03-01', 90);
-INSERT INTO movie (nom, description, date_sortie, duree) VALUES ('test2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam luctus efficitur iaculis. Maecenas eu dolor sit amet nisl pellentesque dapibus. Proin lobortis, odio sit amet commodo mattis, odio ex porttitor nunc, et placerat ex maua tempus eu.', '1990-04-20', 120);
-INSERT INTO movie (nom, description, date_sortie, duree) VALUES ('test3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam luctus efficitur iaculis. Maecenas eu dolor sit amet nisl pellentesque dapibus. Proin lobortis, odio sit amet commodo mattis, odio ex portenenatis magna, a fermentum urna tempus eu.', '2001-09-11', 180);
+INSERT INTO movie (nom, description, date_sortie, duree)
+VALUES ('test',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam luctus efficitur iaculis. Maecenas eu dolor sit amet nisl pellentesque',
+        '2022-03-01', 90);
+INSERT INTO movie (nom, description, date_sortie, duree)
+VALUES ('Dogma',
+        'Loki et Bartleby, deux anges déchus, cherchent à retourner au Paradis. Mais si les deux rénégats parviennent à leur fin, cela signe la fin de la vie sur Terre.  La charmante Bethany est chargée par Metatron, la voix de Dieu, d''empêcher les deux lascars de réaliser leur projet en pénétrant dans une église consacrée du New Jersey. Pour la seconder, elle est accompagnée de deux incroyables prophètes, d''un prétendu treizième apôtre et d''une muse céleste.',
+        '2000-01-19', 130);
+INSERT INTO movie (nom, description, date_sortie, duree)
+VALUES ('Les Tontons flingueurs',
+        'Fernand mène une vie tranquille en tant que négociant de machines agricoles. Mais lorsque son ami Louis, dit le Mexicain, lui demande à larticle de la mort de veiller sur sa fille et de soccuper de ses affaires plus ou moins louches, Fernand doit dire adieu à la tranquillité...',
+        '1963-11-27', 105);
+INSERT INTO movie (nom, description, date_sortie, duree)
+VALUES ('Wayne\'s World',
+        'Deux adolescents ont transformé une cave en studio de télévision et y animent chaque nuit une émission musicale diffusée sur un réseau câblé local.',
+        '1992-10-28', 94);
+
+INSERT INTO movie (nom, description, date_sortie, duree)
+VALUES ('La Guerre des étoiles',
+        'Dans une galaxie lointaine, l\'univers est dominé par l\'Empire galactique avec à sa tête le sombre et impitoyable Dark Vador. Mais la révolte gronde, l\'Alliance rebelle menée par la princesse Leia, s\'empare des plans de l\'Etoile Noire, la base secrète de l\'Empire. Avant d\'être capturée par les forces impériales, la jeune princesse dissimule les plans de l\'Etoile Noire à son droïde R2-D2 avec pour mission de les remettre au Jedi Obi-Wan Kenobi. '
+        ,'1977-10-19', 121);
+
+INSERT INTO movie (nom, description, date_sortie, duree)
+VALUES ('Le Seigneur des Anneaux - La Communauté de l\'anneau',
+        'Le jeune hobbit Frodon Sacquet hérite d''un anneau. Il s''agit de l''Anneau Unique, instrument de pouvoir absolu qui permettrait à Sauron, de régner sur la Terre du Milieu et de réduire en esclavage ses peuples... A moins que Frodon et ses fidèles compagnons ne parviennent à emporter l''Anneau jusqu''en Mordor, lieu où il a été forgé, et à le détruire pour toujours.'
+        ,'2001-12-19', 198);
+
+INSERT INTO movie (nom, description, date_sortie, duree)
+VALUES ('Dune',
+        'L''histoire de Paul Atreides, jeune homme aussi doué que brillant, voué à connaître un destin hors du commun qui le dépasse totalement. Car s''il veut préserver l''avenir de sa famille et de son peuple, il devra se rendre sur la planète la plus dangereuse de l''univers – la seule à même de fournir la ressource la plus précieuse au monde, capable de décupler la puissance de l''humanité. Tandis que des forces maléfiques se disputent le contrôle de cette planète, seuls ceux qui parviennent à dominer leur peur pourront survivre…'
+        ,'2021-09-15', 155);
+
+
