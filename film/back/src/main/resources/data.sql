c48745aead8eb6a5a064bdb15e7a6b1b01782792
BENJAMIN NOUREUX <benjamin.noureux@atos.net>
2023-03-29 16:53:49 +0200

    remove wrong escape characters

--- film/back/src/main/resources/data.sql
+++ film/back/src/main/resources/data.sql
@@ -9,17 +9,17 @@ VALUES ('Les Tontons flingueurs',
         '1963-11-27', 105, 'Comédie');
 
 INSERT INTO movie (nom, description, date_sortie, duree, genre)
-VALUES ('Wayne\'s World',
+VALUES ('Wayne''s World',
         'Deux adolescents ont transformé une cave en studio de télévision et y animent chaque nuit une émission musicale diffusée sur un réseau câblé local.',
         '1992-10-28', 94, 'Comédie');
 
 INSERT INTO movie (nom, description, date_sortie, duree, genre)
 VALUES ('La Guerre des étoiles',
-        'Dans une galaxie lointaine, l\'univers est dominé par l\'Empire galactique avec à sa tête le sombre et impitoyable Dark Vador. Mais la révolte gronde, l\'Alliance rebelle menée par la princesse Leia, s\'empare des plans de l\'Etoile Noire, la base secrète de l\'Empire. Avant d\'être capturée par les forces impériales, la jeune princesse dissimule les plans de l\'Etoile Noire à son droïde R2-D2 avec pour mission de les remettre au Jedi Obi-Wan Kenobi. '
+        'Dans une galaxie lointaine, l''univers est dominé par l''Empire galactique avec à sa tête le sombre et impitoyable Dark Vador. Mais la révolte gronde, l''Alliance rebelle menée par la princesse Leia, s''empare des plans de l''Etoile Noire, la base secrète de l''Empire. Avant d''être capturée par les forces impériales, la jeune princesse dissimule les plans de l''Etoile Noire à son droïde R2-D2 avec pour mission de les remettre au Jedi Obi-Wan Kenobi. '
         ,'1977-10-19', 121, 'Science-fiction');
 
 INSERT INTO movie (nom, description, date_sortie, duree, genre)
-VALUES ('Le Seigneur des Anneaux - La Communauté de l\'anneau',
+VALUES ('Le Seigneur des Anneaux - La Communauté de l''anneau',
         'Le jeune hobbit Frodon Sacquet hérite d''un anneau. Il s''agit de l''Anneau Unique, instrument de pouvoir absolu qui permettrait à Sauron, de régner sur la Terre du Milieu et de réduire en esclavage ses peuples... A moins que Frodon et ses fidèles compagnons ne parviennent à emporter l''Anneau jusqu''en Mordor, lieu où il a été forgé, et à le détruire pour toujours.'
         ,'2001-12-19', 198, 'Fantasy');
 
@@ -39,12 +39,12 @@ VALUES ('Creed 3',
         ,'2023-03-01', 117, 'Drame');
 
 INSERT INTO movie (nom, description, date_sortie, duree, genre) VALUES
-    ('Le Silence des Agneaux', 'Un agent du FBI doit demander l\'aide d\'un tueur en série cannibale pour attraper un autre tueur en série', '1991-02-14', 118, 'Thriller'),
-    ('Le Parrain', 'L\'histoire d\'une famille mafieuse italo-américaine', '1972-03-24', 175, 'Drame'),
-    ('La Liste de Schindler', 'Un homme d\'affaires allemand sauve la vie de nombreux Juifs pendant l\'Holocauste', '1993-12-15', 195, 'Drame historique'),
-    ('Le Roi Lion', 'L\'histoire d\'un jeune lion qui doit reprendre sa place de roi après la mort de son père', '1994-06-15', 88, 'Animation'),
-    ('Pulp Fiction', 'L\'histoire de plusieurs criminels de Los Angeles dont les histoires se croisent', '1994-05-21', 154, 'Comédie noire'),
-    ('Inception', 'Un voleur expérimenté doit implanter une idée dans l\'esprit d\'un PDG en utilisant la technologie de rêve', '2010-07-21', 148, 'Science-fiction'),
+    ('Le Silence des Agneaux', 'Un agent du FBI doit demander l''aide d''un tueur en série cannibale pour attraper un autre tueur en série', '1991-02-14', 118, 'Thriller'),
+    ('Le Parrain', 'L''histoire d''une famille mafieuse italo-américaine', '1972-03-24', 175, 'Drame'),
+    ('La Liste de Schindler', 'Un homme d''affaires allemand sauve la vie de nombreux Juifs pendant l''Holocauste', '1993-12-15', 195, 'Drame historique'),
+    ('Le Roi Lion', 'L''histoire d''un jeune lion qui doit reprendre sa place de roi après la mort de son père', '1994-06-15', 88, 'Animation'),
+    ('Pulp Fiction', 'L''histoire de plusieurs criminels de Los Angeles dont les histoires se croisent', '1994-05-21', 154, 'Comédie noire'),
+    ('Inception', 'Un voleur expérimenté doit implanter une idée dans l''esprit d''un PDG en utilisant la technologie de rêve', '2010-07-21', 148, 'Science-fiction'),
     ('Whiplash', 'Un jeune batteur de jazz ambitieux est poussé à ses limites par son professeur sadique', '2014-10-10', 107, 'Drame'),
     ('Gone Girl', 'Un homme devient le principal suspect dans la disparition de sa femme', '2014-10-01', 149, 'Thriller'),
     ('La La Land', 'Une actrice et un pianiste de jazz tombent amoureux à Los Angeles', '2016-12-07', 128, 'Comédie musicale'),
